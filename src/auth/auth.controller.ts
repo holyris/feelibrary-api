655c1a9d82596006b706e02c253e882303d0e213
holyris <alexandre.thebault77@gmail.com>
2020-12-21 00:28:37 +0100

    FIx serialization on auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
-import { create } from 'domain';
+import { Controller, Post, UseGuards, Body, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
 import { AuthService } from './auth.service';
@@ -7,6 +6,7 @@ import { Public } from './constants';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
   constructor(
     private authService: AuthService
