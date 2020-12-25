0c08e2f2884f09f5ea27153c18990e983473f9e6
Alexandre Thebault <alexandre.thebault77@gmail.com>
2020-12-25 22:48:32 +0100

    Add create() to feelingsService

--- src/modules/feelings/feelings.module.ts
+++ src/modules/feelings/feelings.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feeling } from './feeling.entity';
 import { FeelingsService } from './feelings.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feeling])],
   providers: [FeelingsService]
 })
 export class FeelingsModule {}
--- src/modules/feelings/feelings.service.ts
+++ src/modules/feelings/feelings.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Feeling } from './feeling.entity';
 
 @Injectable()
-export class FeelingsService {}
+export class FeelingsService {
+  constructor(
+    @InjectRepository(Feeling)
+    private feelingRepository: Repository<Feeling>
+  ) { }
+
+  async create(createFeeling: unknown): Promise<Feeling> {
+    const feeling = new Feeling(createFeeling);
+    return this.feelingRepository.save(feeling).catch(err => {
+      if (err.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException(err.message);
+      } else {
+        throw new InternalServerErrorException('unhandled exception: ' + err)
+      }
+    });
+  }
+}
