745082aceeef9cbf25bb6a43aa6bf458dfab69b9
Alexandre Thebault <alexandre.thebault77@gmail.com>
2021-01-09 10:27:11 +0100

    Fix description column type

--- src/modules/movies/movie.entity.ts
+++ src/modules/movies/movie.entity.ts
@@ -10,7 +10,7 @@ export class Movie {
   @Column()
   title: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, type: "text" })
   description: string;
 
   @Column({ nullable: true })
